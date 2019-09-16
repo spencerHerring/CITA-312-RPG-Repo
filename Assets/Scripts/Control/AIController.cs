4b7fd0b2c03bc8d68efa9eec26c76ba10a108770
SpencerHerring <herrin348@morrisville.edu>
2019-09-16 18:11:32 -0400

    L45 Chase Distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                print(gameObject + "Should Chase");
+            }
+
+            
+            
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+
+    }
+}
